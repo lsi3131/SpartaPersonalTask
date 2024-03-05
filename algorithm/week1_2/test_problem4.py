f42ad341e4d16aa58e0cb07ddd97e2846d87470d
sang-il <lsi9393@naver.com>
2024-03-05 09:37:54 +0900

    week1-2 * 문제4, 5 작성 안되어 있는 부분 추가

--- algorithm/week1_2/test_problem4.py
+++ algorithm/week1_2/test_problem4.py
@@ -5,17 +5,16 @@ import random
 import string
 
 
-# https://school.programmers.co.kr/learn/courses/30/lessons/120583
+# https://school.programmers.co.kr/learn/courses/30/lessons/120821
 
-def solution(array, n):
-    counter = collections.Counter(array)
-    return counter[n]
+def solution(num_list):
+    return num_list[::-1]
 
 
 class TestProblem(unittest.TestCase):
     def test_example1(self):
-        self.assertEqual(2, solution([1, 1, 2, 3, 4, 5], 1))
-        self.assertEqual(0, solution([0, 2, 3, 4], 1))
+        self.assertEqual([5, 4, 3, 2, 1], solution([1, 2, 3, 4, 5]))
+        self.assertEqual([3, 2, 1], solution([1, 2, 3]))
 
 
 if __name__ == '__main__':
